a6d91380b868043ae73288cb2fff05480bbfdb2f
Kauan <raidoka546@gmail.com>
2024-09-21 11:59:54 -0400

    starting the function to open modal

--- src/app/page/todo-list.jsx
+++ src/app/page/todo-list.jsx
@@ -1,22 +1,62 @@
+"use client";
+
 import styles from './todo-list.module.scss';
-import logo from '../../../public/focal-point-logo.png'
+import logo from '../../../public/focal-point-logo.png';
 import Image from 'next/image';
+import dayjs from 'dayjs';
+import 'dayjs/locale/pt-br';
+import { useState } from 'react';
 
 export default function Todo() {
+    dayjs.locale('pt-br');
+    const today = dayjs();
+    let todaysDate = today.format('dddd, DD [de] MMMM [de] YYYY');
+    const [dayOfWeek, restOfDate] = todaysDate.split(', ');
+    todaysDate = `${dayOfWeek.charAt(0).toUpperCase() + dayOfWeek.slice(1)}, ${restOfDate}`;
+
+   const [showModal, setShowModal ] = useState(false);
+
+   const handleOpenModal = () => {
+        setShowModal(true);
+    };
+
     return (
-        <header className={styles.header}>
-            <Image
-                src={logo}
-                alt='FocalPoint Logo'
-                width={150}
-                height={36}
-            />
-            <h1>TODO List</h1>
-            <p>
-                Our flight is scheduled on
-                <time datetime="2021-12-20">December 20th, 2021</time> at
-                <time datetime="18:00">6:00pm</time>.
-            </p>
-        </header>
+        <div className={styles.container}>
+            <header className={styles.header}>
+                <Image
+                    src={logo}
+                    alt='FocalPoint Logo'
+                    width={150}
+                    height={36}
+                />
+                <h1 className={styles.welcomeUser}>Bem-Vindo de volta, Marcus</h1>
+                <p className={styles.todaysDate}>
+                    <time dateTime={todaysDate}>{todaysDate}</time>
+                </p>
+            </header>
+            <main className={styles.tasksContainer}>
+                <p>Suas tarefas de hoje</p>
+                <div>
+                    {/* Here will go the tasks */}
+                </div>
+                <p>Tarefas finalizadas</p>
+                <div>
+                    {/* Here will go the finished tasks */}
+                </div>
+            </main>
+            <button 
+                className={styles.addTask} 
+                onClick={setShowModal}
+            >
+            Adicionar nova tarefa
+            </button>
+
+            {showModal && (
+                <div className={styles.openModal}>
+                
+                </div>)
+            }
+            
+        </div>
     )
 }
